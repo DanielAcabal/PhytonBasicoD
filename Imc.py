175b432f33f1e9da624f9e56c801cbcc7515cbd0
DanielAcabal <79229718+DanielAcabal@users.noreply.github.com>
2021-02-17 21:02:17 -0600

    Update Imc.py

--- Imc.py
+++ Imc.py
@@ -1,4 +1,4 @@
-peso = input("¿Cuál es tu peso?")
-estatura = input("¿Cuál es tu estatura?")
+peso = input("¿Cuál es tu peso? en kg:")
+estatura = input("¿Cuál es tu estatura? en metros:")
 imc = round(float(peso)/float(estatura)**2,2)
 print("Tu imc es:"+str(imc))
