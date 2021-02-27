fbb7f2d265b1d54fe46d208627fa3a8973e629cd
DanielAcabal <79229718+DanielAcabal@users.noreply.github.com>
2021-02-26 23:38:44 -0600

    Create Grupos.py

---
+++ Hoja de Trabajo 3/Grupos.py
@@ -0,0 +1,10 @@
+x = input("Ingresa tu nombre: ")
+j = input("Ingresa tu sexo: ")
+x[0].lower
+j[0].lower
+if x[0] < "M" and j[0] == "m":
+    print("Perteneces al grupo A")
+elif x[0] > "N" and j[0] == "h":
+    print("Perteneces al grupo A")
+else:
+    print("Perteneces al grupo B")
