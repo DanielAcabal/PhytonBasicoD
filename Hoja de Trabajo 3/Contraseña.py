ec415ed3ff7950b9f907d63146bdac2caeef754d
DanielAcabal <79229718+DanielAcabal@users.noreply.github.com>
2021-02-26 23:51:20 -0600

    Create Contraseña.py

---
+++ Hoja de Trabajo 3/Contraseña.py
@@ -0,0 +1,6 @@
+x = input("Ingrese la contraseña: ")
+c = input("Ingrese la contraseña para confirmar: ")
+if x.lower() == c.lower():
+    print("Contraseña correcta")
+else :
+    print("Contraseña incorrecta")
